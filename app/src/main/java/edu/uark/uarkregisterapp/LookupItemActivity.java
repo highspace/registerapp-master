86eae76bb99ad01879dcb012cf6b632febdfa299
Justin Ryles <jaryles@uark.edu>
2017-11-13 21:59:09 -0600

    LookupItemActivity

---
+++ app/src/main/java/edu/uark/uarkregisterapp/LookupItemActivity.java
@@ -0,0 +1,10 @@
+package edu.uark.uarkregisterapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Justin Ryles on 11/13/2017.
+ */
+
+public class LookupItemActivity extends AppCompatActivity {
+}
